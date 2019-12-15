630a5f1822d7cf4a1e8e72faee08771e4734141b
lioness3 <jcnailtech3@yahoo.com>
2019-12-14 19:32:00 -0800

    try to test vowels with syntax error

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -2,18 +2,30 @@ class Anagram
   def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
+
   end
 # splits the user input into seperate letters and sorts them alphabetically to see if they are the same
   def recognize
     split_word1 = @word1.downcase.split(//).sort
     split_word2 = @word2.downcase.split(//).sort
-   if split_word1 === split_word2
-     return true
-   end
-   puts split_word1
-   puts split_word2
+
+    if split_word1 === split_word2
+      puts "yes"
+    end
+    puts split_word1
+    puts split_word2
+
+    end
+    def real_word
+      check1 = @world1.split(//)
+      check2 = @world2.split(//)
+      vowel_string = "aeiou"
+      vowel = vowel_string.split(//)
+      check1.include?vowel
+      check2.include?vowel
+    end
   end
-end
+
 
 # word.scrambled == word
 # > false
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -2,12 +2,16 @@ require('rspec')
 require('ruby_logic')
 
 describe('#Anagram')do
-  it('recognizes words as anagrams') do
-    test = Anagram.new('ate','eat')
-  expect(test.recognize).to(eq(true))
-  end
-  it('recognizes words as anagrams case excluded') do
+  # it('recognizes words as anagrams') do
+  #   test = Anagram.new('ate','eat')
+  # expect(test.recognize).to(eq("yes"))
+  # end
+  # it('recognizes words as anagrams case excluded') do
+  #   test = Anagram.new('Ate','Eat')
+  # expect(test.recognize).to(eq("yes"))
+  # end
+  it('recognizes input as a word if it contains vowels') do
     test = Anagram.new('Ate','Eat')
-  expect(test.recognize).to(eq(true))
+  expect(test.real_word).to(eq('yes'))
   end
 end
