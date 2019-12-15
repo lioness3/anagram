c5eeefd147a3a4aa91fbb567ff4b9765a17dfd74
lioness3 <jcnailtech3@yahoo.com>
2019-12-15 15:13:25 -0800

    antigram length test passes

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -2,29 +2,40 @@ class Anagram
   def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
-
+    @split_word1
+    @split_word2
   end
+  attr_reader(:split_word1, :split_word2)
+
 # splits the user input into seperate letters and sorts them alphabetically to see if they are the same
   def recognize
-    split_word1 = @word1.downcase.split(//).sort
-    split_word2 = @word2.downcase.split(//).sort
+    @split_word1 = @word1.downcase.split(//).sort
+    @split_word2 = @word2.downcase.split(//).sort
 
-    if split_word1 === split_word2
-      puts "yes"
-    end
-    puts split_word1
-    puts split_word2
+    if @split_word1 === @split_word2
 
-    end
-    def real_word
-      check1 = @world1.split(//)
-      check2 = @world2.split(//)
-      vowel_string = "aeiou"
-      vowel = vowel_string.split(//)
-      check1.include?vowel
-      check2.include?vowel
+      return "yes"
     end
   end
+
+  #   def real_word
+  #     check1 = @world1.split(//)
+  #     check2 = @world2.split(//)
+  #     vowel_string = "aeiou"
+  #     vowel = vowel_string.split(//)
+  #     if check1.include?vowel && check2.include?vowel
+  #     puts "includes vowel"
+  #   end
+  # end
+
+    def antigram
+      if (@split_world1.to_s.length) == (@split_world2.to_s.length)
+          "equal length"
+      elsif
+          'These words are Antigrams'
+    end
+  end
+end
 
 
 # word.scrambled == word
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -2,16 +2,21 @@ require('rspec')
 require('ruby_logic')
 
 describe('#Anagram')do
-  # it('recognizes words as anagrams') do
-  #   test = Anagram.new('ate','eat')
-  # expect(test.recognize).to(eq("yes"))
-  # end
-  # it('recognizes words as anagrams case excluded') do
-  #   test = Anagram.new('Ate','Eat')
-  # expect(test.recognize).to(eq("yes"))
-  # end
-  it('recognizes input as a word if it contains vowels') do
+  it('recognizes words as anagrams') do
+    test = Anagram.new('ate','eat')
+  expect(test.recognize).to(eq("yes"))
+  end
+  it('recognizes words as anagrams case excluded') do
     test = Anagram.new('Ate','Eat')
-  expect(test.real_word).to(eq('yes'))
+  expect(test.recognize).to(eq("yes"))
+  end
+  # it('recognizes input as a word if it contains vowels') do
+  #   test = Anagram.new('Ate','Eat')
+  # expect(test.real_word).to(eq('includes vowels'))
+  # end
+  it('recognize that two words are equal length') do
+    test = Anagram.new('Ate','Eat')
+    test.recognize()
+    expect(test.antigram).to(eq("equal length"))
   end
 end
