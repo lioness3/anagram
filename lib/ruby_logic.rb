014e6b087fc82f25c3187f2d7759f571b656a94a
lioness3 <jcnailtech3@yahoo.com>
2019-12-19 15:02:08 -0800

    add the correct files to repo

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -1,27 +1,32 @@
+require('pry')
 class Anagram
-  attr_accessor(:split_word1, :split_word2, :vowel_count1, :vowel_count2)
+  attr_accessor(:split_word1, :split_word2, :word1, :word2)
   def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
     @split_word1
     @split_word2
-    @vowel_count1
-    @vowel_count2
   end
-
+#Deletes spaces ina  asentance and splits string by words
   def delete_spaces
-     @word1.split(/\W/)
-     @word2.split(/\W/)
+     @word1 = @word1.to_s.split(/\W/)
+     @word2 = @word2.to_s.split(/\W/)
+     return @word1, @word2
   end
-
-# splits the user input into seperate letters and sorts them alphabetically to see if they are the same
+# compares word lengths to determine if words can be Antigrams
+  def antigram
+    if (@split_world1.to_s.length) === (@split_world2.to_s.length)
+      return 'These words have no letter matches and are antigrams'
+    end
+  end
+# splits the user input into seperate letters and sorts them alphabetically to see if they are the same word regardless of case
   def recognize
-    @split_word1 = @word1.downcase.split(//).sort
-    @split_word2 = @word2.downcase.split(//).sort
-
+    @split_word1 = @word1.to_s.downcase.split(//).sort
+    @split_word2 = @word2.to_s.downcase.split(//).sort
     if @split_word1 === @split_word2
-      return "yes"
-
+      return "These are Anagrams"
+    else
+      self.antigram
     end
   end
 # searches all entered characters for vowels
@@ -31,12 +36,4 @@ class Anagram
     'Enter an actual word'
    end
  end
-# compares word lengths to determine if words can be Antigrams
-  def antigram
-    if (@split_world1.to_s.length) === (@split_world2.to_s.length)
-     "equal length"
-    elsif
-    'These words are Antigrams'
-    end
-  end
 end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -4,16 +4,16 @@ require('ruby_logic')
 describe('#Anagram')do
   it('recognizes words as anagrams') do
     test = Anagram.new('ate','eat')
-  expect(test.recognize).to(eq("yes"))
+  expect(test.recognize).to(eq("These are Anagrams"))
   end
   it('recognizes words as anagrams case excluded') do
     test = Anagram.new('Ate','Eat')
-  expect(test.recognize).to(eq("yes"))
+  expect(test.recognize).to(eq("These are Anagrams"))
   end
   it('recognize that two words are equal length') do
-    test = Anagram.new('Ate','Eat')
+    test = Anagram.new('red','cat')
     test.recognize()
-    expect(test.antigram).to(eq("equal length"))
+    expect(test.antigram).to(eq('These words have no letter matches and are antigrams'))
   end
   it('recognizes input as a word if it contains vowels') do
     test = Anagram.new('jjj','trk')
@@ -22,6 +22,11 @@ describe('#Anagram')do
   end
   it('recognizes a sentance without spacing or punctucation') do
     test = Anagram.new('We went here','Or they go there')
-    expect(test.delete_spaces).to(eq(['Or',"they","go","there"]))
+    expect(test.delete_spaces).to(eq([["We", "went", "here"], ["Or", "they", "go", "there"]]))
+  end
+  it('tests with sentance') do
+    test = Anagram.new('We went here','Or they stop')
+    test.recognize
+    expect(test.delete_spaces).to(eq([["We", "went", "here"],['Or',"they","stop"]]))
   end
 end
